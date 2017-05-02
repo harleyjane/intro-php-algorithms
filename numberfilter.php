ad9b5a38c16c2ac3136170ee22f963edd0542bda
harleyjane <harleyjane@hotmail.com>
2017-05-02 16:38:43 +0000

    cleans up for loop and conditional; adds notes for next steps

--- numberfilter.php
+++ numberfilter.php
@@ -10,20 +10,26 @@ All of your code should go inside this function's { and } braces. -->
 
 function numberFilterer(){
     $our_numbers = array(30, 5, 9, 10, 11);
+    $arr = array();
+    
         for ($x = 0; $x <= count($our_numbers); $x++) {
-            if ($our_numbers[$x] <= 10) {
-                array_push(NAMEOFARRAY, $our_numbers[$x])
-            else {      
+            if ($our_numbers[$x] < 10) {
+                array_push($arr, $our_numbers[$x]);
             }
-            
-            // think about where this empty array should be defined
         } 
         
-    // return the new list
-    return ();
+    print_r($arr);
+   
+    
 }
 
+numberFilterer();
+
+// NEXT STEPS:
 // change the function so that the value of the array is not hard coded within the function, but passed into the function as an argument.
+// output on line 21 will change to `return` instead of `print_r`
+// line 26 will be removed.
+// code comments in tests `/*` and `*/` will also be removed.
 ?>
 
 <!-- Below, you'll find the code that checks for correctness of your work.
@@ -33,8 +39,8 @@ what your function should do.
 
 BUT DO NOT MODIFY THE BELOW CODE AT ALL. -->
 
-<?php
-
+ <?php
+/*
 $testInput1 = array(30, 5, 9, 10, 11);
 $result1    = numberFilterer(testInput1);
 
@@ -65,5 +71,5 @@ else {
     echo("Expected to get: 0, 8, 9, 9");
     echo("Instead, got: " + implode(", ", $result2));
 }
-
+*/
 ?>
